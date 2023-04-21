19a8d5ff65806f3c070de03702f7fbe101c5aa44
loftdoggy <chtori1202@gmail.com>
2023-04-22 03:06:17 +0900

    Initial commit

---
+++ Assets/Word Finder/Scripts/WordContainer.cs
@@ -0,0 +1,33 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordContainer : MonoBehaviour
+{
+    [Header(" Elements ")]
+    private LetterContainer[] letterContainers;
+
+    private void Awake()
+    {
+        letterContainers = GetComponentsInChildren<LetterContainer>();
+        Initialize();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Initialize()
+    {
+        for (int i = 0;  i < letterContainers.Length; i++)
+            letterContainers[i].Initialize();
+        
+    }
+}
